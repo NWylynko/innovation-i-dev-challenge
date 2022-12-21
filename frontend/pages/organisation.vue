9a5361e6cc5daf542d762221fcdcd881f585bee8
Nick Wylynko <nick@wylynko.com>
2022-12-21 11:33:48 +0800

    Create Page for Organisations

---
+++ frontend/pages/organisation.vue
@@ -0,0 +1,58 @@
+<template>
+  <v-card>
+    <v-card-title
+      >Organisation List <v-btn icon @click="dialog = true"><v-icon>mdi-plus</v-icon></v-btn></v-card-title
+    >
+    <v-data-table :headers="headers" :items="organisations">
+      <template v-slot:item.active="{ item }">
+        <v-btn icon @click="deleteOrganisation(item.id)"><v-icon>mdi-delete</v-icon></v-btn>
+      </template>
+    </v-data-table>
+    <v-dialog v-model="dialog">
+      <organisation-form
+        @save="
+          dialog = false
+          getOrganisations()
+        "
+      />
+    </v-dialog>
+  </v-card>
+</template>
+
+<script>
+import OrganisationForm from '../components/OrganisationForm.vue'
+export default {
+  components: { OrganisationForm },
+
+  data() {
+    return {
+      dialog: false,
+      organisations: [],
+      headers: [
+        { text: 'Name', value: 'name' },
+        { text: 'Address', value: 'address' },
+        { text: 'City', value: 'city' },
+        { text: 'State', value: 'state' },
+        { text: 'Country', value: 'country' },
+        { text: 'Postcode', value: 'postcode' },
+        { text: 'Phone', value: 'phone' },
+        { text: 'Email', value: 'email' },
+        { text: 'Website', value: 'website' },
+        { text: 'Active', value: 'active' },
+      ],
+    }
+  },
+  created() {
+    this.getOrganisations()
+  },
+  methods: {
+    async getOrganisations() {
+      this.organisations = await this.$axios.$get('/organisation/organisations')
+    },
+    async deleteOrganisation(id) {
+      await this.$axios.$delete('/organisation/organisation', { params: { id } })
+      this.getOrganisations()
+    },
+  },
+}
+</script>
