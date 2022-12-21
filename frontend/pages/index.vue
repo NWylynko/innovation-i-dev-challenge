1a2bd3d754577a565006a66435e3bd82931108cf
Nick Wylynko <nick@wylynko.com>
2022-12-21 12:29:59 +0800

    Add links to pages on home page

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,4 +1,9 @@
-<template></template>
+<template>
+  <main>
+    <a href="/employee">Employees</a>
+    <a href="/organisation">Organisations</a>
+  </main>
+</template>
 
 <script>
 export default {}
